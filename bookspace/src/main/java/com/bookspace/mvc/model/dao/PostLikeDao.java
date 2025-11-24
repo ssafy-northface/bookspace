c1292a6a9a3a0a7d932f7f1f6a262ca87b32b9d8
seoyeong5864 <wendy2281@naver.com>
2025-11-25 00:30:15 +0900

    [Feat] DAO 작성 (#4)

--- bookspace/src/main/java/com/bookspace/mvc/model/dao/PostLikeDao.java
+++ bookspace/src/main/java/com/bookspace/mvc/model/dao/PostLikeDao.java
@@ -1,8 +1,25 @@
 package com.bookspace.mvc.model.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.bookspace.mvc.model.dto.PostLike;
 
 @Mapper
 public interface PostLikeDao {
-
+	
+	// 게시글에 좋아요 남기기
+	int insertLike(PostLike like);
+	
+	// 남긴 좋아요 삭제
+	int deleteLike(long likeId);
+	
+	// 좋아요 개수 카운트 (postId 기준)
+	int countLikeByPostId(long postId);
+	
+	// 이미 좋아요를 눌렀는지 확인
+	// int로 반환해도 OK
+	boolean existLike(@Param("postId") long postId, @Param("userId") long userId);
+	
+	
 }
--- bookspace/src/main/java/com/bookspace/mvc/model/dao/ReviewDao.java
+++ bookspace/src/main/java/com/bookspace/mvc/model/dao/ReviewDao.java
@@ -1,8 +1,25 @@
 package com.bookspace.mvc.model.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.bookspace.mvc.model.dto.Review;
 
 @Mapper
 public interface ReviewDao {
 
+	// 리뷰를 작성
+	int insertReview(Review review);
+	
+	// 리뷰를 조회 (bookId 기준)
+	List<Review> selectReviewByBookId(long bookId);
+	
+	// 리뷰를 수정
+	int updateReview(Review review);
+	
+	// 리뷰를 삭제 (soft delete)
+	int deleteReview(long reviewId);
+	
+	
 }
